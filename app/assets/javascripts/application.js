93dcd0b4ff1f2d8ef886b674145fc726a087826f
Khanh Nguyen <khanh@intersect.org.au>
2015-01-12 19:19:38 +1100

    Refactor food diary javascript + Add 'add food' button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,5 +37,92 @@ function ready() {
     });
 }
 
+function addFoodToPlate(foodElement) {
+    var foodId = foodElement.data('food-id');
+    var foodName = foodElement.data('food-name');
+    var swapTip = foodElement.data('swap-tip');
+    $('.current-plate').append(foodElement.clone()
+        .find('img')
+        .attr('height', '75')
+        .attr('width', '75')
+        .addClass('food-on-plate img-circle')
+        .attr('data-toggle', 'tooltip')
+        .attr('data-placement', 'top')
+        .attr('title', foodName).attr('id', foodId));
+
+    var trHtml = '<tr class="food" data-food-id="'+ foodId +'"><td>' + foodName + '</td></tr>';
+    $('.current-plate-details .delete-after-first').hide();
+    $('.current-plate-details').find('tbody').append(trHtml);
+
+    updateFoodCounter();
+
+    $('[data-toggle="tooltip"]').tooltip()
+
+    setRemoveFood($('.current-plate').children().last());
+
+    if (swapTip != "") {
+        $('.tip').html(swapTip);
+    }
+}
+
+
+function setDrag(obj) {
+    obj.draggable({
+        revert: "invalid",
+        helper: "clone",
+        cursor: "move"
+    });
+}
+
+function setAllFoodRemove() {
+    $('.food-on-plate').on('click', function(e) {
+        removeFood($(this));
+    });
+}
+
+function setRemoveFood(food) {
+    food.on('click', function(e) {
+        removeFood($(this));
+    })
+}
+
+function applyPlateOnClick() {
+    $('plate').on('click', function(e) {
+        if ($(this).hasClass('strobe')) {
+            return;
+        }
+        else {
+            togglePlates($(this).attr('id'));
+        }
+    })
+}
+
+function togglePlates(id) {
+    $('.strobe').removeClass('strobe')
+    $('.current-plate-details').removeClass('current-plate-details').addClass('hidden');
+    $('.current-plate').removeClass('current-plate').addClass('hidden');
+
+    $('plate[id='+ id +']').addClass('strobe');
+    $('.plate-detail[id='+ id +']').removeClass('hidden').addClass('current-plate-details');
+    $('.food-goes-here[id='+ id +']').removeClass('hidden').addClass('current-plate');
+}
+
+function updateFoodCounter() {
+    var foodCounter = $('.strobe .food-counter');
+    foodCounter.html($('.current-plate').find('img').length);
+}
+
+function removeFood(food) {
+    var foodId = food.attr('id');
+    food.remove();
+    $('.current-plate-details tr.food[data-food-id='+ foodId +']').first().remove();
+    if ($('.current-plate-details tr.food').length < 1) {
+        $('.current-plate-details .delete-after-first').show();
+    }
+    updateFoodCounter();
+
+    $('.tooltip.in').hide();
+}
+
 $(document).ready(ready)
 $(document).on('page:load', ready)
